4ff838eee5981e00be06479f0ffd757c9710286c
Domenic Catalano <domenic.catalano@dcmail.ca>
2020-11-11 11:30:54 -0500

    added simple validation for user's age

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -3,8 +3,10 @@
  * Date: November 11, 2020
  * Program Name: Lesson 9
  */
+
 package ca.durhamcollege;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main
@@ -12,8 +14,8 @@ public class Main
 
     public static void main(String[] args)
     {
-        // Define our scanner
-        Scanner	keyboard = new Scanner(System.in);
+        // Scanner definition
+        Scanner keyboard = new Scanner(System.in);
 
         // INPUT
         String name = null;
@@ -21,13 +23,37 @@ public class Main
         name = keyboard.nextLine();
 
         float age = 0.0f;
-        System.out.print("Please enter your age: ");
-        age = keyboard.nextFloat();
+        boolean isValidInput = false; // sentinel variable
+
+        do
+        {
+            System.out.print("Please enter your age: ");
+            try
+            {
+
+                age = keyboard.nextFloat();
+                isValidInput = true;
+
+                if((age <= 0.0f) || (age >= 120.0f))
+                {
+                    System.out.println("Error: You must enter an age greater than 0.0 and less than 120.0 years old");
+                    keyboard.nextLine();
+                    isValidInput = false;
+                }
+            }
+            catch(InputMismatchException inputMismatchException)
+            {
+                System.out.println("Error: You must enter a valid floating-point number");
+                keyboard.nextLine();
+                isValidInput = false;
+            }
+
+        }
+        while(!isValidInput);
 
         // OUTPUT
         System.out.println();
-        System.out.printf("You entered: %s \n", name);
-        System.out.printf("You entered: %,.2f \n", age);
-
+        System.out.printf("You Entered: %s \n",name);
+        System.out.printf("You Entered: %.2f \n",age);
     }
 }
