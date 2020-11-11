afe8504f9f45b68112bf98a9ece4404ea991e367
Domenic Catalano <domenic.catalano@dcmail.ca>
2020-11-11 10:40:05 -0500

    input & output test with two variables

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Author: Domenic Catalano
  * Date: November 11, 2020
  * Program Name: Lesson 9
@@ -12,10 +12,22 @@ public class Main
 
     public static void main(String[] args)
     {
-        String name = null;
+        // Define our scanner
         Scanner	keyboard = new Scanner(System.in);
+
+        // INPUT
+        String name = null;
         System.out.print("Please enter your name: ");
         name = keyboard.nextLine();
-        System.out.printf("You entered: %s", name);
+
+        float age = 0.0f;
+        System.out.print("Please enter your age: ");
+        age = keyboard.nextFloat();
+
+        // OUTPUT
+        System.out.println();
+        System.out.printf("You entered: %s \n", name);
+        System.out.printf("You entered: %,.2f \n", age);
+
     }
 }
